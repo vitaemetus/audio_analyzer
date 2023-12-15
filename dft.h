d9928b93c7fcc6fd4faa014ca12c0fa9e863da48
vitaemetus <bazaliy.daniil@gmail.com>
2023-12-15 23:30:27 +0300

    logarithmic freq distribution

--- dft.h
+++ dft.h
@@ -6,14 +6,18 @@
 
 std::vector<std::complex<float>> dft (std::vector<std::complex<float>> X){
     int N = X.size();
-    int bins = 10;
+
+    std::vector<float> bins =  {5.0, 13.0, 32.0,
+                                79.0, 200.0, 500.0,
+                                1255.0, 3155.0, 7920.0, 
+                                20000.0};
 
     std::vector<std::complex<float>> output;
-    output.reserve(bins);
+    output.reserve(bins.size());
 
     std::complex<float> sum;
 
-    for (int freq = 20; freq < 20000; freq++){
+    for (float freq : bins){
         sum = std::complex<float>(0, 0);
         for (int n  = 0; n < N; n++){
             float Real = cos(2 * M_PI * freq * (static_cast<float> (n) / static_cast<float> (N)));
