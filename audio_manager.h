a551727dc4aa781b2e42c9b2a1edfce6313ac0c4
furs-aka-beast <kiruha200409@gmail.com>
2023-12-16 00:03:54 +0300

    audio_manager v1

--- audio_manager.h
+++ audio_manager.h
@@ -3,22 +3,46 @@
 #include <cstdlib>
 #include <string>
 #include "AudioFile/AudioFile.h"
+#include <vector>
+#include <chrono>
+#include <thread>
 struct audio_manager {
     audio_manager(std::string filename): filename(filename){
         audioFile.load (filename);
+        framesamp=audioFile.getSampleRate();
+        if(framesamp!=audioFile.getSampleRate()) std::cout<<"wrong fps";
     }
     audio_manager(){audio_manager{"test.wav"};}
     void play(){
+        start=std::chrono::system_clock::now();
         system(("afplay " + filename).c_str());
+
+    }
+    std::vector <float> get_samples(){
+
+        current_sample+=framesamp;
+        return std::vector <float> (audioFile.samples[0].begin()+(current_sample-framesamp), audioFile.samples[0].begin()+current_sample);
+    }
+    bool wait(){
+        std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
+        if(diff.count()+1>=lengthInSeconds) return 0;
+        else if(diff.count()<current_sample/fps){
+            int delay = (current_sample/fps-diff.count())*1e6;
+            std::this_thread::sleep_for(std::chrono::microseconds(delay));
+        }
+        else {
+            std::cout<<"late";
+        }
+        return 1;
     }
     void summ(){audioFile.printSummary();}
-    void compress(int samps){
-        if(samps<audioFile.getSampleRate()){
-            audioFile.setNumSamplesPerChannel (samps);
-        }
-        else throw "Less samples than original";
-    }
+
     private:
     std::string filename;
     AudioFile<double> audioFile;
+    int framesamp;
+    int fps=30;
+    int current_sample=0;
+    std::chrono::time_point<std::chrono::system_clock> start;
+    double lengthInSeconds = audioFile.getLengthInSeconds();
 };
