87b0ac96809a5b08e85ebcc03e3803ae5e361eca
vitaemetus <bazaliy.daniil@gmail.com>
2023-12-16 02:29:09 +0300

    printing for a single frame done

--- dft.h
+++ dft.h
@@ -4,27 +4,23 @@
 #include <complex>
 #include <iostream>
 
-std::vector<std::complex<float>> dft (std::vector<std::complex<float>> X){
+std::vector<float> dft (std::vector<std::complex<float>> X){
     int N = X.size();
+    int K = round(N / 2);
 
-    std::vector<float> bins =  {5.0, 13.0, 32.0,
-                                79.0, 200.0, 500.0,
-                                1255.0, 3155.0, 7920.0, 
-                                20000.0};
-
-    std::vector<std::complex<float>> output;
-    output.reserve(bins.size());
+    std::vector<float> output;
+    output.reserve(K);
 
     std::complex<float> sum;
 
-    for (float freq : bins){
+    for (int k = 0; k < K; k++){
         sum = std::complex<float>(0, 0);
         for (int n  = 0; n < N; n++){
-            float Real = cos(2 * M_PI * freq * (static_cast<float> (n) / static_cast<float> (N)));
-            float Imag = sin(2 * M_PI * freq * (static_cast<float> (n) / static_cast<float> (N)));
+            float Real = cos(2 * M_PI * k * (static_cast<float> (n) / static_cast<float> (N)));
+            float Imag = sin(2 * M_PI * k * (static_cast<float> (n) / static_cast<float> (N)));
             sum += X[n] * std::complex<float>(Real, -Imag);
         }
-        output.push_back(sum);
+        output.push_back(sqrt(pow(sum.real()/N, 2) + pow(sum.imag()/N, 2)));
     }
 
     return output;
