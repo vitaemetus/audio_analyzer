26bf4e024c64b32f2a7f5a8ca2c48161301da5e3
furs-aka-beast <kiruha200409@gmail.com>
2023-12-15 22:28:35 +0300

    haha

--- audio_manager.h
+++ audio_manager.h
@@ -1,14 +1,22 @@
 #pragma once
+#include <iostream>
 #include <cstdlib>
 #include <string>
 #include "AudioFile/AudioFile.h"
 struct audio_manager {
     audio_manager(std::string filename): filename(filename){
-        audioFile.load ("test.wav");
+        audioFile.load (filename);
     }
-    audio_manager(){audio_manager{"test.wav"}}
+    audio_manager(){audio_manager{"test.wav"};}
     void play(){
         system(("afplay " + filename).c_str());
+    }
+    void summ(){audioFile.printSummary();}
+    void compress(int samps){
+        if(samps<audioFile.getSampleRate()){
+            audioFile.setNumSamplesPerChannel (samps);
+        }
+        else throw "Less samples than original";
     }
     private:
     std::string filename;
--- main.cpp
+++ main.cpp
@@ -1,1 +1,6 @@
-#include<audio_manager.h>
+#include "audio_manager.h"
+
+int main(){
+    audio_manager AM;
+    AM.summ();
+}
