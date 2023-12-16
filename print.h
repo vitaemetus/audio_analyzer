1c3455c6bc2fd68ea2b84e8dc04396534108812b
vitaemetus <bazaliy.daniil@gmail.com>
2023-12-16 15:17:46 +0300

    dknkljfnfdkjgnd

--- print.h
+++ print.h
@@ -16,9 +16,11 @@ void print (std::vector<float> signal){
     std::vector<float> Fx = dft(signal);
 
     //Mean by bin:
+    std::cout<<"\r";
     std::cout<<"\n"<<"Freq\t"<<std::setw(6)<<"Magn"<<std::endl;
 
     float max = *std::max_element(Fx.begin(), Fx.end());
+
 
     for (int i = 0; i < n_bins; i++){
         std::vector<float>::const_iterator first = Fx.begin() + i*50;
@@ -28,7 +30,7 @@ void print (std::vector<float> signal){
 
         float average = 10 * (*std::max_element(sliced_Fx.begin(), sliced_Fx.end())) / (max);
 
-        std::cout<<"\r"<<i*50<<":\t"<<std::setw(6);
+        std::cout<<i*50<<":\t"<<std::setw(6);
         for (int b = 0; b < average; b++){
             std::cout<<"#";
         }
